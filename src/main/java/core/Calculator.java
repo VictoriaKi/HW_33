f9be506c46ab9881da9f0653fc5920315e4e7754
Barack Obama <barack.obama@gmail.com>
2018-05-04 13:03:04 -0700

    Repository HW_33 added

---
+++ src/main/java/core/Calculator.java
@@ -0,0 +1,71 @@
+package core;
+
+public class Calculator {
+
+	static double add(double a, double b) {
+		return a + b;
+	}
+
+	static double add(double a, double b, double c) {
+		return a + b + c;
+	}
+
+	static double add(double a, double b, double c, double d) {
+		return a + b + c + d;
+	}
+
+	static double divide(double a, double b) {
+		return a / b;
+	}
+
+	static double divide(double a, double b, double c) {
+		return a / b / c;
+	}
+
+	static double divide(double a, double b, double c, double d) {
+		return a / b / c / d;
+	}
+
+	static double multiply(double a, double b) {
+		return a * b;
+	}
+
+	static double multiply(double a, double b, double c) {
+		return a * b * c;
+	}
+
+	static double multiply(double a, double b, double c, double d) {
+		return a * b * c * d;
+	}
+
+	static double subtract(double a, double b) {
+		return a - b;
+	}
+
+	static double subtract(double a, double b, double c) {
+		return a - b - c;
+	}
+
+	static double subtract(double a, double b, double c, double d) {
+		return a - b - c - d;
+	}
+	
+	static double avg(double a, double b) {
+		return (a + b)/2;
+	}
+	
+	static double avg(double a, double b, double c) {
+		return (a + b + c)/3;
+	}
+	
+	static double avg(double a, double b, double c, double d) {
+		return (a + b + c + d)/4;
+	}
+	
+	
+	
+	
+	static double min(double a, double b, double c, double d) {
+		return a - b - c - d;
+	}
+}
